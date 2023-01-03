3e8b8244a966ae3bedbaf4ebd9a45be6bb186be0
Danil Herr <herr.danil@yandex.com>
2023-01-03 22:25:28 +0300

    First Commit

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,74 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01-big.jpg";
+
+import project02 from "./../img/projects/02.jpg";
+import project02Big from "./../img/projects/02-big.jpg";
+
+import project03 from "./../img/projects/03.jpg";
+import project03Big from "./../img/projects/03-big.jpg";
+
+import project04 from "./../img/projects/04.jpg";
+import project04Big from "./../img/projects/04-big.jpg";
+
+import project05 from "./../img/projects/05.jpg";
+import project05Big from "./../img/projects/05-big.jpg";
+
+import project06 from "./../img/projects/06.jpg";
+import project06Big from "./../img/projects/06-big.jpg";
+
+// import project06 from "./../img/projects/07.jpg";
+// import project06Big from "./../img/projects/07-big.jpg";
+
+const projects = [
+    {
+        title: 'Interactive website design',
+        skills: 'JavaScript, CSS, HTML',
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: 'https://github.com/IDanilHerr/JsScrollSite',
+    },
+    {
+        title: 'Local ToDo list',
+        skills: 'JavaScript, CSS, HTML',
+        img: project02,
+        imgBig: project02Big,
+        gitHubLink: 'https://github.com/IDanilHerr/todo',
+    },
+    {
+        title: 'Website design',
+        skills: 'JavaScript, CSS, HTML',
+        img: project03,
+        imgBig: project03Big,
+        gitHubLink: 'https://github.com/IDanilHerr/JsSwipeSite',
+    },
+    {
+        title: 'Equipment store',
+        skills: 'JavaScript, CSS, HTML',
+        img: project04,
+        imgBig: project04Big,
+        gitHubLink: 'https://github.com/IDanilHerr/electronics-store-design',
+    },
+    {
+        title: 'Сopy of wikipedia page',
+        skills: 'HTML, CSS',
+        img: project05,
+        imgBig: project05Big,
+        gitHubLink: 'https://github.com/IDanilHerr/copy-of-wikipedia-page',
+    },
+    {
+        title: 'Site сarousel',
+        skills: 'React, CSS, HTML',
+        img: project06,
+        imgBig: project06Big,
+        gitHubLink: 'https://github.com/IDanilHerr/ReactSiteCarousel',
+    },
+    // {
+    //     title: 'Site сarousel',
+    //     skills: 'React, CSS, HTML',
+    //     img: project07,
+    //     imgBig: project07Big,
+    //     gitHubLink: 'https://github.com/IDanilHerr/ReactSiteCarousel',
+    // },
+];
+
+export {projects}
---
+++ src/pages/Contacts.js
@@ -0,0 +1,27 @@
+const Contacts = () => {
+    return (
+        <main className="section">
+        <div className="container">
+                <h1 className="title-1">Contacts</h1>
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Location</h2>
+                        <p>Istanbul, Turkey</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Telegram / WhatsApp</h2>
+                        <p><a href="tel:+79051234567">+7 (922) 666-07-00</a></p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Email</h2>
+                        <p><a href="mailto:Idanilherr@gmail.com">Idanilherr@gmail.com</a></p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+    );
+}
+ 
+export default Contacts;

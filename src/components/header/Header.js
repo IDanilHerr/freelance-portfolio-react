354f8dfa14be299b5def752921e0495fd3ff6657
Danil Herr <herr.danil@yandex.com>
2023-01-06 22:19:53 +0300

    added download link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                 <div className="header__text">
                     <p>with passion for learning and creating.</p>
                 </div>
-                <a href="#!" className="btn">
+                <a href="https://drive.google.com/uc?export=download&id=1qRgipJDyfPRmB5R6C-bPKMfoM7JDOj74" className="btn">
                     Download CV
                 </a>
             </div>
